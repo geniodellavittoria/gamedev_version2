3e7c934c550066f9f244d77f14ea7945ad0ae94d
Tim Bolzern <t.bolzern@gmail.com>
2018-12-02 21:28:30 +0100

    added Hero ScriptableObject

---
+++ Assets/ScriptableObjects/Hero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu (fileName="New Hero", menuName="Hero")]
+    class Hero: ScriptableObject
+    {
+        public string Name;
+        public int Jumping;
+        public int Speed;
+        public int Strength;
+        public int Life;
+    }
+}
