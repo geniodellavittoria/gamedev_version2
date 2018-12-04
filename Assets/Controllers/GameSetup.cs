ed6b5008c183668b00cca98dbbe92fb133149cac
Tim Bolzern <t.bolzern@gmail.com>
2018-12-04 20:51:07 +0100

    added HeroSelection

---
+++ Assets/Controllers/GameSetup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Controllers
+{
+    public class GameSetup: MonoBehaviour
+    {
+        public Transform hero;
+        public void OnLoadScene()
+        {
+            Instantiate(hero, new Vector2(0, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Controllers/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject HeroMenu;
+	public void OnPlayClick()
+    {
+        MainMenu.SetActive(false);
+        HeroMenu.SetActive(true);
+    }
+}
