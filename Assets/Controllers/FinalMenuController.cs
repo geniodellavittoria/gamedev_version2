8817d46b334ed4d527144a0c869cfe4fe55e51a5
Tim Bolzern <t.bolzern@gmail.com>
2018-12-05 22:00:57 +0100

    made all buttons the same and made texts diffrent

---
+++ Assets/Controllers/FinalMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalMenuController : MonoBehaviour {
+    private LoadSceneOnClick loadScene = new LoadSceneOnClick();
+	public void OnBackButtonClick()
+    {
+        // load main menu scene
+        loadScene.LoadByIndex(0);
+    }
+
+    public void OnHighscoreButtonClick()
+    {
+        // load highscore scene
+        loadScene.LoadByIndex(0);
+    }
+}
